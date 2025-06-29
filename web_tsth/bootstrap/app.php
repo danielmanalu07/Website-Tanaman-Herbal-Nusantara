0c297937a125685e420c6bf02f9c58d28f817853
danielmanalu07 <danielmanalu101@gmail.com>
2025-06-30 00:11:07 +0700

    Update Code

--- web_tsth/bootstrap/app.php
+++ web_tsth/bootstrap/app.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -8,10 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-// ->withMiddleware(function (Middleware $middleware) {
-//     //
-// })
-// ->withExceptions(function (Exceptions $exceptions) {
-//     //
-// })
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
     ->create();
