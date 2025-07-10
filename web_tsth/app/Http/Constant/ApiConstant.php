403648475bd3733203962842006c3554f7672db0
danielmanalu07 <danielmanalu101@gmail.com>
2025-07-10 12:13:06 +0700

    Update Base URL Web

--- web_tsth/app/Http/Constant/ApiConstant.php
+++ web_tsth/app/Http/Constant/ApiConstant.php
@@ -3,7 +3,7 @@ namespace App\Http\Constant;
 
 class ApiConstant
 {
-    public const BASE_URL = 'http://192.168.185.215:8002/api';
+    public const BASE_URL = 'http://service-taman.tsth2.web.id/api';
     // public const BASE_URL = 'http://localhost:8002/api';
     // public const BASE_URL = 'https://carefree-tranquility-production-fb66.up.railway.app/api';
 }
